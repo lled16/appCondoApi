4c6856ebc077a553fa413f29881b1dee8faa560f
simao <vitoralves1604@gmail.com>
2025-08-06 00:28:05 -0300

    Configure files / gitignore

--- AppCondo.Domain/Login/JwtSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AppCondo.Domain.Login
-{
-    public class JwtSettings
-    {
-        public string Key { get; set; }
-        public string Issuer { get; set; }
-        public string Audience { get; set; }
-    }
-}
--- AppCondo.Domain/Login/User.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AppCondo.Domain.Login
-{
-    public class User
-    {
-        public int Id { get; set; }
-        public string Nome { get; set; }
-        public int Tipo { get; set; }
-    }
-}
